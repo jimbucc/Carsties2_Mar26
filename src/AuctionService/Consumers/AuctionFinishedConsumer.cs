5f52b5dd45a7146e33f201810d4107639095e544
jimbucc <James.Bucciferro@PERKINELMER.COM>
2026-03-05 12:09:08 -0500

    End of Section 6

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,28 @@
+using System;
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer(AuctionDbContext auctionDbContext) : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("---> Consuming AuctionFinished");
+        var auction = await auctionDbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if(context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = 
+            auction.SoldAmount > auction.ReservedPrice 
+            ? Entities.Status.Finished 
+            : Entities.Status.ReserveNotMet;
+            
+        await auctionDbContext.SaveChangesAsync();
+    }
+}
--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -18,8 +18,7 @@ public static class Config
         };
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
-        {
+        [
             new Client
             {
                 ClientId = "postman",
@@ -28,6 +27,44 @@ public static class Config
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600 * 24 * 30    // 1 month
             }
-        };
+        ];
 }
+
+
+// new Client[]
+//         {
+//             new Client
+//             {
+//         ClientId = "postman",
+//                 ClientName = "Postman",
+//                 AllowedScopes = { "openid", "profile", "auctionApp"},
+//                 RedirectUris = { "https://www.getpostman.com/oauth2/callback"},
+//                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
+//                 AllowedGrantTypes = { GrantType.ResourceOwnerPassword}
+//             },
+//             new Client
+//             {
+//                 ClientId = "nextApp",
+//                 ClientName = "nextApp",
+//                 ClientSecrets = { new Secret("secret".Sha256()) },
+//                 AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+//                 RequirePkce = false,
+//                 RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+//                 AllowOfflineAccess = true,
+//                 AllowedScopes = { "openid", "profile", "auctionApp" },
+//                 AccessTokenLifetime = 3600 * 24 * 30    // 1 month
+//             }
+//         };
